2b8eb4a6dcd235fd0bdb0ec7b8df0089b2d3454f
Tatsunori Saito <bbrfkr@gmail.com>
2025-05-03 23:02:46 +0000

    fix mouse button assign

--- firmware/qmk/laser/keymaps/via/keymap.c
+++ firmware/qmk/laser/keymaps/via/keymap.c
@@ -25,7 +25,7 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ansi(
-        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_BTN1,    KC_BTN2,
+        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_BTN2,    KC_BTN1,
         KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                KC_ENT,
         KC_LSFT,                KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    KC_UP,
         KC_LCTL,    KC_LGUI,    KC_LALT,                MO(1),                  MO(2),                  KC_SPC,                 KC_RALT,    KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_BTN3
